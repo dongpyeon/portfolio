f53c73adfe1948e2b5502096d371a2ac48124bf3
김동현 <kd0948@naver.com>
2024-03-11 19:27:31 +0900

    2024.03.01 포트폴리오 사진삽입

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,29 +1,39 @@
 import ContactItem from "../ContactItem";
 import Introduce from "./Introduce";
-
+import  myprofile from "../../../public/images/Profile/myprofile.jpg";
 import { DataProps } from "@/types";
+import Image from "next/image";
 
 const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <h1 className="leading-[1.15]">
-          {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요,
-          <br /> 개발자를 꿈꾸는{" "}
-          <span className="text-PRIMARY font-semibold">{information.name}</span>
-          입니다.
-        </h1>
-        <div className="flex gap-1">
-          {information.contact.map((contact) => (
-            <ContactItem
-              key={contact.id}
-              className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
-              {...contact}
-            >
-              {contact.name}
-            </ContactItem>
-          ))}
+        <div>
+          <Image
+            src="/images/Profile/myprofile.jpg"
+            alt="myprofile"
+            width="200"
+            height="200"
+            className="object-cover rounded-lg border-[1px] border-GRAY_LIGHT dark:border-white border-soli "
+          />
+          <h1 className="leading-[1.15]">
+            {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
+            안녕하세요,
+            <br /> 개발자를 꿈꾸는{" "}
+            <span className="text-PRIMARY font-semibold">{information.name}</span>
+            입니다.
+          </h1>
+          <div className="flex gap-1">
+            {information.contact.map((contact) => (
+              <ContactItem
+                key={contact.id}
+                className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
+                {...contact}
+              >
+                {contact.name}
+              </ContactItem>
+            ))}
+          </div>
         </div>
       </div>
       <Introduce markdown={information.markdown} />
