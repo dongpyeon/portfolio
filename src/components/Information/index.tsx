edb2e497220ab2353030d5f37fb1515dc333c035
김동현 <143480675+dongpyeon@users.noreply.github.com>
2024-03-12 16:49:54 +0900

    2024/03/12 포트폴리오

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -1,21 +1,22 @@
 import ContactItem from "../ContactItem";
 import Introduce from "./Introduce";
-import  myprofile from "../../../public/images/Profile/myprofile.jpg";
 import { DataProps } from "@/types";
 import Image from "next/image";
 
 const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-2">
+      <div className="flex items-center gap-8"> {/* 여기서 flex 및 items-center를 사용하여 이미지와 내용을 수평으로 정렬합니다 */}
         <div>
           <Image
             src="/images/Profile/myprofile.jpg"
             alt="myprofile"
-            width="200"
-            height="200"
-            className="object-cover rounded-lg border-[1px] border-GRAY_LIGHT dark:border-white border-soli "
+            width="230"
+            height="230"
+            className="object-cover rounded-lg border-[1px] border-GRAY_LIGHT dark:border-white border-soli"
           />
+        </div>
+        <div>
           <h1 className="leading-[1.15]">
             {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
             안녕하세요,
